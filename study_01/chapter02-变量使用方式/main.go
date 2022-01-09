74a87272e7a6cacbd5b3c5bde7d8b80f4c72e85b
yanyinuo <1601530253@qq.com>
2022-01-09 19:39:59 +0800

    1-9

--- study_01/chapter02-变量使用方式/main.go
+++ study_01/chapter02-变量使用方式/main.go
@@ -28,15 +28,27 @@ func main() {
 */
 
 //该案例演示golang如何一次性声明多个变量
+
+var n1 = 100
+var n2 = 200
+var name = "jack"
+var (
+	n3 = 300
+	n4 = 900
+	name2="mary"
+)
+
 func main() {
 	// var n1, n2,n3 int
 	// fmt.Println("n1=",n1,"n2=", n2,"n3=", n3)
 
 	//一次性声明多个变量的方式2
-	var n1,name , n3 = 100,"tom", 888
-	fmt.Println("n1=",n1,"name=", name,"n3=",n3)
+	// var n1,name , n3 = 100,"tom", 888
+	// fmt.Println("n1=",n1,"name=", name,"n3=",n3)
 
 	//一次性声明多个变量的方式3，同样可以使用类型推导
 	// n1, name , n3 := 100,"tom~", 888
 	// fmt.Println( "n1=",n1,"name=", name,"n3=", n3)
+
+	fmt.Println( "n1=",n1,"n2=",n2,"name=", name,"n3=", n3,"n4=",n4)
 }
