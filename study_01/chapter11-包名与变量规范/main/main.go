29d0b00cd854ab067be5cd80a274be72aa66316f
yanyinuo <1601530253@qq.com>
2022-01-13 21:03:39 +0800

    1-13

---
+++ study_01/chapter11-包名与变量规范/main/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"go_code/go_study/study_01/chapter11-包名与变量规范/model"
+)
+
+func main() {
+	fmt.Println("maintest", model.HeroName)
+}
---
+++ study_01/chapter11-包名与变量规范/model/until.go
@@ -0,0 +1,5 @@
+package model
+
+//变量首字母大写是公开的，小写是私有的
+// var heroName string = "yzy"
+var HeroName string = "yzy"
