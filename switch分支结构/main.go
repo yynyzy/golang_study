680ce8b38b637534c070e5cc1012d29136cfab95
yanyinuo <1601530253@qq.com>
2022-01-18 23:52:11 +0800

    1-18

--- switch分支结构/main.go
+++ switch分支结构/main.go
@@ -11,7 +11,11 @@ switch细节讨论
 3) case后面可以带多个表达式，使用逗号间隔。比如case表达式1,表达式2..
 4) case后面的表达式如果是常量值(字面量)，则要求不能重复
 5) case后面不需要带break ,程序匹配到一个case后就会执行对应的代码块，然后退出switch，如果一个都匹配不到，则执行default
-6)default语句不是必须的.
+6)default语句不是必须的
+7) switch后也可以不带表达式，类似if --else分支来使用。
+8)switch后也可以直接声明/定义一个变量，分号结束，不推荐。
+9）switch穿透-fallthrough，如果在case语句块后增加 fallthrough ,则会继续执行下一个case（只会穿透一层）,也叫switch穿透。
+
 */
 func main() {
 	/*
