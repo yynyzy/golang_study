b9fd3f075ae9c8edc0d76f9d7f9b77d7b5cdd66d
yanyinuo <1601530253@qq.com>
2022-01-20 00:16:03 +0800

    1-20

--- for循环/main.go
+++ for循环/main.go
@@ -6,20 +6,20 @@ import (
 
 func main() {
 
-	for i := 1; i <= 10; i++ {
-		fmt.Println("你好，尚硅谷", i)
+	for i := 1; i <= 5; i++ {
+		fmt.Println("你好", i)
 	}
 	// for循环的第二种写法
-	j := 1        //循环变量初始化
-	for j <= 10 { //循环条件
-		fmt.Println("你好，尚硅谷~", j)
+	j := 1       //循环变量初始化
+	for j <= 5 { //循环条件
+		fmt.Println("你好", j)
 		j++ //循环变量迭代
 	}
 
 	//for循环的第三种写法,这种写法通常会配合break使用
 	k := 1
 	for { //这里等价 for ;;{
-		if k <= 10 {
+		if k <= 3 {
 			fmt.Println("ok~", k)
 		} else {
 			break //跳出循环
@@ -27,10 +27,19 @@ func main() {
 		k++
 	}
 
-	//字符串遍历方式一
+	//字符串遍历方式一传统方式
+	/*
+		有中文会乱码，因为传统的对字符串的遍历是按照字节来遍历，而一个汉字在 utf8 编码对应的是 3个字节。所以输出一个字节就会乱码。
+		解决方法：使用 []rune切片
+	*/
+	// var str string = "握手,yzy"
+	// for i := 0; i < len(str); i++ {
+	// 	fmt.Printf("%c\n", str[i])
+	// }
 	var str string = "握手,yzy"
-	for i := 0; i < len(str); i++ {
-		fmt.Printf("%c\n", str[i])
+	str2 := []rune(str)
+	for i := 0; i < len(str2); i++ {
+		fmt.Printf("%c\n", str2[i])
 	}
 
 	//字符串遍历方式二——for range
