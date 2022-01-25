813b6c0465a5225db6978ce5f1f1cddb5b803f59
yanyinuo <1601530253@qq.com>
2022-01-25 14:37:09 +0800

    1-25

--- 06-函数/main.go
+++ 06-函数/main.go
@@ -6,14 +6,14 @@ func addSum(num1 int, num2 int) int {
 	return num1 + num2
 }
 
-//函数是一种数据类型，可以赋值给一个变量
+//1.函数是一种数据类型，可以赋值给一个变量
 //函数既然是一种数据类型，因此在Go中，函数可以作为形参，并且调用
 func myFun(funvar func(int, int) int, num1 int, num2 int) int {
 	return funvar(num1, num2)
 }
 
 /*
- Go支持自定义数据类型
+ 2.Go支持自定义数据类型
 基本语法： type 自定义数据类型名 数据类型 //相当取了一个别名
 如：type myint int
 如：type myFunType  func(int, int)int //这时 myFunType 就等价于一个函数类型 func(int, int)int
@@ -24,11 +24,33 @@ func myFun1(funvar myFunType, num1 int, num2 int) int {
 	return funvar(num1, num2)
 }
 
-//支持对函数返回值命名
+/*
+  3.支持对函数返回值命名
+*/
 func getSumandSub(n1 int, n2 int) (sum int, sub int) {
 	sub = n1 - n2
 	sum = n1 + n2
 	return
+}
+
+/*
+	4.GO支持可变参数
+	//支持0到多个参数
+	func sum(args...int) sum int{
+	}
+
+	//支持1到多个参数
+	func sum(n1 int,args...int) sum int{
+	}
+
+	(1) agrs 是 slice切片，通过args[index] 可以访问到各个值
+*/
+func sum(n1 int, args ...int) (sum int) {
+	sum = n1
+	for i := 0; i < len(args); i++ {
+		sum += args[i]
+	}
+	return sum
 }
 
 func main() {
@@ -40,4 +62,7 @@ func main() {
 	fmt.Printf("变量a1的类型是%T，c1的值是%d\n", a1, c1)
 	x, y := getSumandSub(4, 3)
 	fmt.Println("变量x，y是]n\n", x, y)
+
+	res := sum(1, 2, 3)
+	fmt.Println("4.sum求和", res)
 }
