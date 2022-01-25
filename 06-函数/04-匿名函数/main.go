cde6f5da94bbe35739e6e9e2b691e0ab7f2a677d
yanyinuo <1601530253@qq.com>
2022-01-25 19:32:47 +0800

    1-25

---
+++ 06-函数/04-匿名函数/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+/*
+ 如果某个函数只希望使用一次，可以考虑使用匿名函数，匿名函数也可以实现多次调用
+*/
+
+func main() {
+	//将匿名函数 func(n1 int, n2 int) int 赋给 res1 这个变量，此时可以通过res1这个变量调用
+	res1 := func(n1 int, n2 int) int {
+		return n1 + n2
+	}(1, 3)
+	fmt.Println("res1=", res1)
+}
