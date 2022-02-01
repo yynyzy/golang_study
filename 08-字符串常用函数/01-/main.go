395d325b4d3db38fbefb586f0c7968385bcb1562
yanyinuo <1601530253@qq.com>
2022-02-01 21:00:17 +0800

    2-1

--- 08-字符串常用函数/01-/main.go
+++ 08-字符串常用函数/01-/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"strconv"
+	"strings"
 )
 
 func main() {
@@ -31,14 +32,26 @@ func main() {
 	str = strconv.Itoa(121)
 	fmt.Printf("str=%v,str=%T\n", str, str)
 
-	//5)字符串转[]byte: var bytes = []byte("hello go")
+	//5) 字符串转[]byte: var bytes = []byte("hello go")
 	var bytes = []byte("he1lo go")
 	fmt.Printf("bytes=%v\n", bytes)
-	//6)[]byte转字符串:str = string([]byte[97,98,99})
+
+	//6) []byte转字符串:str = string([]byte[97,98,99})
 	str = string([]byte{97, 98, 99})
 	fmt.Printf("str=%v\n", str)
-	//10进制转2，8，16进制:str = strconv.FormatInt(123，2),返回对应的字符串
+
+	//7) 10进制转2，8，16进制:str = strconv.FormatInt(123，2),返回对应的字符串
 	str = strconv.FormatInt(123, 2)
-	fmt.Printf("123对应的二进制是=%v", str)
+	fmt.Printf("123对应的二进制是=%v\n", str)
+	str = strconv.FormatInt(123, 16)
+	fmt.Printf("123对应的十六进制是=%v\n", str)
+
+	//8)查找子串是否在指定的字符串中: strings.Contains("seafood","foo"') //true
+	b := strings.Contains("seafood", "mary")
+	fmt.Printf("b=%v\n", b)
+
+	//9)统计一个字符串有几个指定的子串:strings.count(" ceheese","e")//4
+	num := strings.Count("ceheese", "se")
+	fmt.Printf("num=%v\n", num)
 
 }
