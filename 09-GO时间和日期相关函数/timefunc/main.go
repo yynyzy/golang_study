de9d91a3d3f1971a331cd040214c36c553372095
yanyinuo <1601530253@qq.com>
2022-02-03 18:30:37 +0800

    2-3

---
+++ 09-GO时间和日期相关函数/timefunc/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	//看看日期和时间相关函数和方法使用//1．获取当前时间
+	now := time.Now()
+	fmt.Printf("now=%v now type=%T\n", now, now)
+	//2.通过now可以获取到年月日,时分秒
+	fmt.Printf("年=%v\n", now.Year())
+	fmt.Printf("月=%v\n", now.Month())
+	fmt.Printf("月=%v\n", int(now.Month()))
+	fmt.Printf("日=%v\n", now.Day())
+	fmt.Printf("时=%v\n", now.Hour())
+	fmt.Printf("分=%v\n", now.Minute())
+	fmt.Printf("秒=%v\n", now.Second())
+}
