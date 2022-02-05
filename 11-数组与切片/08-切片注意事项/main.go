3191f7c701a35ccaebcfd6107cf08099a7a186d7
yanyinuo <1601530253@qq.com>
2022-02-05 21:47:53 +0800

    2-5

--- 11-数组与切片/08-切片注意事项/main.go
+++ 11-数组与切片/08-切片注意事项/main.go
@@ -22,9 +22,9 @@ func main() {
 	/*
 	   切片append操作的底层原理分析:
 	   1)切片append操作的本质就是对数组扩容
-	   2) go底层会创建一下新的数组newArr(安装扩容后大小)
-	   3)将slice原来包含的元素拷贝到新的数组newArr
-	   4) slice重新引用到newArr
+	   2) go底层会创建一下新的数组newArr(如果没有超过cap时，扩容直接在尾部追加，当新数组长度超过了cap时才会拥有新的地址)
+	   3)将slice原来包含的元素拷贝到新的数组newArr（或原数组）
+	   4) slice重新引用到newArr（或原数组）
 	   5)注意newArr是在底层来维护的，程序员不可见.
 	**/
 
@@ -37,4 +37,13 @@ func main() {
 	slice = append(slice, slice...)
 	fmt.Printf("第三次slice:%v,slice的地址=%p\n", slice, &slice)
 
+	//内置copy函数将第二个切片复制到第一个切片，两个参数都必须是切片
+	var slice3 = []int{1, 2, 3, 4, 5}
+	slice4 := make([]int, 10)
+	copy(slice4, slice3)
+	fmt.Println("slice3", slice3)
+	fmt.Println("slice4", slice4)
+	slice3[1] = 100
+	fmt.Println("slice3", slice3)
+	fmt.Println("slice4", slice4)
 }
