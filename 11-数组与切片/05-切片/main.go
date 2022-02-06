457f6a275f89165454e135108dd5e94e83272623
yanyinuo <1601530253@qq.com>
2022-02-06 21:17:33 +0800

    2-6

--- 11-数组与切片/05-切片/main.go
+++ 11-数组与切片/05-切片/main.go
@@ -7,13 +7,13 @@ import "fmt"
 **/
 func main() {
 	//演示切片的基本使用
-	var intArr [5]int = [...]int{1, 22, 33, 66, 99} //声明/定义一个切片
+	var intArr [5]int = [...]int{1, 22, 33, 66, 99} //声明一个数组
 
-	//slice := intArr[1:3]
+	//定义一个切片
+	slice := intArr[1:3]
 	//1. slice就是切片名
 	//2. intArr[1:3]表示slice引用到intArr这个数组
 	//3．引用intArr数组的起始下标为1,最后的下标为3(但是不包含3)
-	slice := intArr[1:3]
 	fmt.Println("intArr 的值=", intArr)
 	fmt.Println("slice 的元素=", slice)
 	fmt.Println("slice 元素的个数=", len(slice))
