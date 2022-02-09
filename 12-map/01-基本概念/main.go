ca8c489eb75b705d1feff1aff81dffa34a5a2306
yanyinuo <1601530253@qq.com>
2022-02-09 20:28:16 +0800

    2-9

---
+++ 12-map/01-基本概念/main.go
@@ -0,0 +1,13 @@
+package main
+
+/*
+#基本语法
+var map 变量名  map[keytype]valuetype
+## key 可以是什么类型
+golang中的map，的 key可以是很多种类型，比如 bool，数字，string，指针，channel ，还可以是只包含前面几个类型的接口，结构体,数组。通常为int 、 string
+注意: slice, map还有 function不可以，因为这几个没法用 ==来判断
+
+**/
+func main() {
+
+}
