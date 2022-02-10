d2c85424b1c8b0449012d297ea8b485a87f4b82c
yanyinuo <1601530253@qq.com>
2022-02-10 21:42:57 +0800

    2-10

--- 12-map/05-map切片/main.go
+++ 12-map/05-map切片/main.go
@@ -4,10 +4,13 @@ import "fmt"
 
 func main() {
 	monster := make([]map[string]string, 2)
-	if monster == nil {
+	if monster[0] == nil {
+		monster[0] = make(map[string]string, 2)
 		monster[0]["name"] = "yyn"
 		monster[0]["age"] = "18"
-
+	}
+	if monster[1] == nil {
+		monster[1] = make(map[string]string, 2)
 		monster[1]["name"] = "yzy"
 		monster[1]["age"] = "17"
 	}
