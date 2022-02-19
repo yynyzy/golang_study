5887a4a082f31817fea554210c4a02cbeb9142f1
yanyinuo <1601530253@qq.com>
2022-02-19 21:06:58 +0800

    2-19

--- 13-结构体/02-字段细节/main.go
+++ 13-结构体/02-字段细节/main.go
@@ -4,6 +4,9 @@ import "fmt"
 
 //如果结构体的字段类型是:指针，slice，和map的零值都是nil ，即还没有分配空间
 //如果需要使用这样的字段,需要先make，才能使用.
+
+//不同结构体变量的字段是独立，互不影响，一个结构体变量字段的更改，不影响另外一个。
+
 type Person struct {
 	Name   string
 	Age    int
@@ -26,6 +29,8 @@ func main() {
 	if p1.map1 == nil {
 		fmt.Println("ok3")
 	}
+
+	//使用 slice 和 map 一定要先赋值
 	p1.slice = []int{1, 2, 3}
 	fmt.Println(p1.slice)
 	p1.ptr = new(int)
