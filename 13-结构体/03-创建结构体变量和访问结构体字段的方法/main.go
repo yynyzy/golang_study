1f04d3b74580b516ae0122f4f4e2c25193e30d12
yanyinuo <1601530253@qq.com>
2022-02-19 21:48:47 +0800

    2-19

--- 13-结构体/03-创建结构体变量和访问结构体字段的方法/main.go
+++ 13-结构体/03-创建结构体变量和访问结构体字段的方法/main.go
@@ -14,7 +14,7 @@ func main() {
 	p2 := Person{"mary", 20}
 	// p2.Name = "tom"
 	// p2.Age = 18
-	fmt.Println(p2)
+	fmt.Println("p2=", p2)
 
 	//方式3-&
 	//案例:var person *Person = new (Person)
@@ -28,6 +28,19 @@ func main() {
 
 	(*p3).Age = 30
 	p3.Age = 100
-	fmt.Println(*p3)
+	fmt.Println("p3=", *p3)
+
+	//方式4-{}
+	var person *Person = &Person{}
+	//因为person是一个指针，因此标准的访间字段的方法
+	// (*person) .Name =”scott"
+	// go的设计者为了程序员使用方便，也可以 person.Name ="scott"
+	//原因和上面一样，底层会对 person.Name =scott" 进行处理， 会加上 (*person)
+	(*person).Name = "scott"
+	person.Name = "scott~~"
+
+	(*person).Age = 88
+	person.Age = 10
+	fmt.Println("person=", *person)
 
 }
