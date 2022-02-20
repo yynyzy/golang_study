e8ee1d3eae09be20b6417bac38fe10601b98a27a
yanyinuo <1601530253@qq.com>
2022-02-20 14:42:37 +0800

    2-20

--- 13-结构体/05-使用细节/main.go
+++ 13-结构体/05-使用细节/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"unsafe"
+)
 
 /*
 	1.结构体的所有字段在内存中是连续的
@@ -24,7 +27,7 @@ func main() {
 
 	//r1有四个 int，在内存中连续分配
 	fmt.Printf("r1.leftUp.x的地址=%p,r1.leftUp.y的地址=%p\nr1.rightDown.x的地址=%p,r1.rightDown.y的地址=%p\n", &r1.leftUp.x, &r1.leftUp.y, &r1.rightDown.x, &r1.rightDown.y)
-
+	fmt.Printf("的字节数是%d\n", unsafe.Sizeof(r1.leftUp))
 	r2 := Rect2{&Point{1, 2}, &Point{3, 4}}
 
 	//r2有两个*Point类型，这个两个*Point类型的本身地址也是连续的;
