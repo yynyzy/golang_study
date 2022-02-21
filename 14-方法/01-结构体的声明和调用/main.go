9c79f1c0dbeb427e136a85edc7180d48f4bee683
yanyinuo <1601530253@qq.com>
2022-02-21 16:26:14 +0800

    2-21

--- 14-方法/01-结构体的声明和调用/main.go
+++ 14-方法/01-结构体的声明和调用/main.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 
 // 10.2.2方法的声明和调用
+
 type Person struct {
 	Name string
 }
@@ -17,18 +18,22 @@ func (p Person) add(n1, n2 int) int {
 	return res
 }
 
-//对上面的语法的说明
+/*
+	方法的调用和传参机制原理:(重要!)
+	说明:方法的调用和传参机制和函数基本一样，不一样的地方是方法调用时，会将调用方法的变量，当做实参也传递给方法。下
+*/
 
 //1) func(p Person) test{}   表示 Person 结构体有一方法，方法名为test
 //2) (p Person)体现 test方法是和 Person 类型绑定的
-//3） test 方法只能通过 Perso 类型的变量来调用，而不能直接调用，也不能使用其他类型来调用
+//3） test 方法只能通过 Person 类型的变量来调用，而不能直接调用，也不能使用其他类型来调用
 func main() {
 	var p Person
 
 	p.Name = "Tom"
-	// test()   //错误的调用,必须使用 p.test() 的用法
-	p.test()                      //调用方法，把 p 传进去
-	fmt.Println("main()", p.Name) //这里的 p 是副本，值传递所以不影响外面的
+	// test()  错误的调用,必须使用 p.test() 的用法
+
+	p.test()                      //调用方法，把 p的副本传进去
+	fmt.Println("main()", p.Name) //"Tom"（和 p.test() 不影响）
 
 	res := p.add(1, 2)
 	fmt.Println("res=", res)
