0781872440ad0140768404e65e881549019fbfe1
yanyinuo <1601530253@qq.com>
2022-02-23 17:30:40 +0800

    2-23

---
+++ 16-面向对象三大特性/02-继承/extenddemo/main.go
@@ -0,0 +1,53 @@
+package main
+
+import "fmt"
+
+//1.小学生
+type Student struct {
+	Name  string
+	Age   int
+	Score int
+}
+
+//显示他的成绩
+func (stu *Student) ShowInfo() {
+	fmt.Printf("学生名=%v年龄=%v成绩=%v\n", stu.Name, stu.Age, stu.Score)
+}
+func (stu *Student) SetScore(score int) {
+	stu.Score = score
+}
+
+//1.小学生
+type Pupil struct {
+	Student //嵌入了 Student 的匿名结构体
+}
+
+func (p *Pupil) testing() {
+	fmt.Println("小学生正在考试.....")
+}
+
+//1.大学生
+type Graduate struct {
+	Student //嵌入了 Student 的匿名结构体
+}
+
+func (p *Graduate) testing() {
+	fmt.Println("大学生正在考试.....")
+}
+
+func main() {
+	//当我们对结构体嵌入了匿名结构体使用方法会发生变化
+	pupil := &Pupil{}
+	pupil.Student.Name = "tom~"
+	pupil.Student.Age = 8
+	pupil.testing()
+	pupil.Student.SetScore(70)
+	pupil.Student.ShowInfo()
+	graduate := &Graduate{}
+	graduate.Student.Name = "mary~"
+	graduate.Student.Age = 28
+	graduate.testing()
+	graduate.Student.SetScore(99)
+	graduate.Student.ShowInfo()
+
+}
