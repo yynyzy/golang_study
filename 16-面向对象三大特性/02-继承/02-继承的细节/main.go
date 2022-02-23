13aeeca329b6150d97b5b6deacb4b60fc4095d81
yyn <1601530253@qq.com>
2022-02-23 21:07:59 +0800

    2-23

--- 16-面向对象三大特性/02-继承/02-继承的细节/main.go
+++ 16-面向对象三大特性/02-继承/02-继承的细节/main.go
@@ -21,7 +21,24 @@ type C struct {
 type D struct {
 	a A
 }
+type Goods struct{
+	Name string
+	Price float64
+}
+type Brand struct{
+	Name string
+	Address string
+}
 
+type TV struct{
+	Goods
+	Brand
+}
+
+type TV2 struct{
+	*Goods
+	*Brand
+}
 func main() {
 	var c C
 	//如果c没有Name字段，而 A 和 B 有 Name, 这时就必须通过指定匿名结构体名字来区分
@@ -34,4 +51,31 @@ func main() {
 	//比如d.a . Name
 	var d D
 	d.a.Name = "jack"
+
+
+	test2()
+	
+}
+
+func test2 (){
+	//嵌套匿名结构体后，也可以在创建结构体变量(实例)时，直接指定各个匿名结构体字段的值
+tv := TV{Goods{"电视机001", 5000.99}, Brand{"海尔", "山东"}, }
+tv2 := TV{
+Goods{
+Price : 5000.99,
+Name : "电视机002" ,
+},
+Brand{
+Name : "夏普",
+Address :"北京" ,
+},
+
+}
+fmt. Println("tv", tv)
+fmt . Println("tv2", tv2)
+
+
+tv3 := TV2{ &Goods{"电视机003", 7000.99}, &Brand{"创维", "湖南"}, }
+fmt.Println("tv3",*tv3.Goods,*tv3.Brand)
+
 }
