c54bdb92d0f94cf20fc639f9307a8a6f09fc2036
yanyinuo <1601530253@qq.com>
2022-02-23 14:09:20 +0800

    2-23

--- 15-工厂模式/main/main.go
+++ 15-工厂模式/main/main.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"fmt"
+	"go_code/go_study/15-工厂模式/model"
 )
 
 func main() {
-	stu := model.student{
-		Name:  "严以诺",
-		Score: 21,
-	}
-	fmt.Println(stu)
+	// stu := model.student{
+	// 	Name:  "严以诺",
+	// 	Score: 21,
+	// }
+
+	stu := model.NewStudent("yzy", 18)
+	//student结构体首字母是小写，可以通过工厂模式来解决
+	fmt.Println(*stu)
 }
--- 15-工厂模式/model/student.go
+++ 15-工厂模式/model/student.go
@@ -7,3 +7,9 @@ type student struct {
 
 //因为 student 结构体首字母是小写，因此只能子啊model使用
 //我们通过工厂模式来解决
+func NewStudent(name string, score float64) *student {
+	return &student{
+		Name:  name,
+		Score: score,
+	}
+}
