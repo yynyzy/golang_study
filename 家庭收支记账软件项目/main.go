1c681b527b1d1701ea2306eac94e37ed1710797f
yanyinuo <1601530253@qq.com>
2022-02-26 16:28:50 +0800

    2-26

--- 家庭收支记账软件项目/main.go
+++ 家庭收支记账软件项目/main.go
@@ -5,6 +5,14 @@ import "fmt"
 func main() {
 	//声明一个变量，保存接收用户输入的选项
 	key := ""
+	//账户余额
+	// balance := 10000.0
+	//每次收支金额
+	// money := 0.0
+	//每次收支说明
+	// note := ""
+	//收支明细
+	details := "收支\t账户金额\t收支金额\t说  明"
 	//显示这个主菜单
 table:
 	for {
@@ -18,6 +26,7 @@ table:
 		switch key {
 		case "1":
 			fmt.Println("进入收支明细")
+			fmt.Println(details)
 
 		case "2":
 			fmt.Println("进入登记收入")
