a512b983a227ed8161bfbf17c510ba1ebe4247f4
yyn <1601530253@qq.com>
2022-02-27 19:43:38 +0800

    2-27

---
+++ 18-文件/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+/*
+流:数据在数据源(文件)和程序(内存)之间经历的路径
+输入流:数据从数据源(文件)到程序(内存)的路径
+输出流:数据从程序(内存)到数据源(文件)的路径
+*/
+
+func main() {
+	//file 可以叫 file对象，file句柄，file指针
+	file, err := os.Open("./test.txt")
+	if err != nil {
+		fmt.Println("Open file error!")
+	} else {
+		fmt.Printf("%p\n", file) //是个指针
+	}
+	err = file.Close()
+	if err != nil {
+		fmt.Println("文件打开失败")
+	}
+}
