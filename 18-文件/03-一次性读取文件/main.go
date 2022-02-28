907aecfdffdaa19eafd798757c9a69bc7b2ee875
yyn <1601530253@qq.com>
2022-02-28 17:04:53 +0800

    2-28

---
+++ 18-文件/03-一次性读取文件/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+)
+
+func main() {
+	/*
+		ioutil 一次性读取文件到内存，前提是文件不大的情况下
+		ioutil.ReadFile 不需要open和close ，因为 ReadFile 已经把它们全部完成了
+	*/
+	content, err := ioutil.ReadFile("../test.txt")
+	if err != nil {
+		fmt.Println("打开文件失败！")
+	} else {
+		fmt.Println(content)
+	}
+}
