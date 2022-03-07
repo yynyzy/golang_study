b079fd4a75f522fab7afcf69d6c26acd52515d7e
yanyinuo <1601530253@qq.com>
2022-03-07 16:55:52 +0800

    3-7

--- 19-JSON序列化/02-反序列化/main.go
+++ 19-JSON序列化/02-反序列化/main.go
@@ -13,42 +13,34 @@ type Monster struct {
 func UnmarshalStruct() {
 	var monster Monster
 	str := "{\"Name\":\"严以诺\",\"Age\":18}"
-	data, err := json.Unmarshal([]byte(str), &monster)
+	err := json.Unmarshal([]byte(str), &monster)
 	if err != nil {
 		fmt.Printf("UnmarshalStruct err=%v\n", err)
 	} else {
-		fmt.Printf("monster反序列化%v\n", string(data))
+		fmt.Printf("monster反序列化%v\n", monster)
 	}
 }
 
-func TestMap() {
+func UnmarshalMap() {
 	//定义一个map
-	var demo map[string]interface{} = make(map[string]interface{})
-	demo["name"] = "阿斯顿"
-	demo["age"] = 20
-	data, err := json.Marshal(&demo)
+	var demo map[string]interface{}
+	str := "{\"Name\":\"嘤嘤嘤\",\"Age\":20}"
+	err := json.Unmarshal([]byte(str), &demo)
 	if err != nil {
-		fmt.Printf("err=%v\n", err)
+		fmt.Printf("UnmarshalMap err=%v\n", err)
 	} else {
-		fmt.Printf("Map序列化%v\n", string(data))
+		fmt.Printf("Map反序列化%v\n", demo)
 	}
 }
 
-func TestSlice() {
+func UnmarshalSlice() {
 	var sliceArray []map[string]interface{}
-	var s1 = make(map[string]interface{})
-	s1["name"] = "北京"
-	s1["age"] = 99
-	var s2 = make(map[string]interface{})
-	s2["name"] = "湖南"
-	s2["age"] = 88
-
-	sliceArray = append(sliceArray, s1, s2)
-	data, err := json.Marshal(&sliceArray)
+	str := "[{\"Name\":\"啧啧啧\",\"Age\": 49},{\"Name\":\"啊啊啊\",\"Age\": 88}]"
+	err := json.Unmarshal([]byte(str), &sliceArray)
 	if err != nil {
-		fmt.Printf("err=%v\n", err)
+		fmt.Printf("UnmarshalSlice err=%v\n", err)
 	} else {
-		fmt.Printf("切片序列化%v\n", string(data))
+		fmt.Printf("切片反序列化%v\n", sliceArray)
 	}
 }
 
