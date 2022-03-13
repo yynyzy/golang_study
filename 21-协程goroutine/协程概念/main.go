0a333d3978072d77a9fe8e6a5efe77282e634922
yyn <1601530253@qq.com>
2022-03-13 20:28:25 +0800

    3-13

--- 21-协程goroutine/协程概念/main.go
+++ 21-协程goroutine/协程概念/main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"fmt"
+	"strconv"
+	"time"
+)
+
 /*
 Go协程和Go主线程
 1) Go主线程(有程序员直接称为线程/也可以理解成进程):一一个Go线程上，可以起
@@ -11,3 +17,22 @@ Go协程和Go主线程
 	·调度由用户控制
 	·协程是轻量级的线程
 */
+
+/*
+	1.在go主线程中开启一个 goroutine，每隔一秒输出一个 helloworld
+	2.在go主线程中也每隔一秒输出一个 helloworld
+*/
+
+func test() {
+	for i := 0; i < 5; i++ {
+		fmt.Println("test() hello World" + strconv.Itoa(i))
+		time.Sleep(time.Second)
+	}
+}
+func main() {
+	go test() //开启协程
+	for i := 0; i < 5; i++ {
+		fmt.Println("main() hello World" + strconv.Itoa(i))
+		time.Sleep(time.Second)
+	}
+}
