b835864a58ee11ea57eefcf1b5a2b0e9ad379892
yyn <1601530253@qq.com>
2022-03-16 00:52:57 +0800

    3-16

--- 21-协程goroutine/03协程可能引发的问题/main.go
+++ 21-协程goroutine/03协程可能引发的问题/main.go
@@ -2,16 +2,20 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
 /*
 	计算1～100 各个数的阶乘，把结果放入到 map 中
 
-	问题：会出现两个协程同时修改一个map【key】的 值
+	问题：1.会出现两个协程同时修改一个map【key】的 值
+		 2.无法确定程序需要延迟多少秒来让协程执行
 */
 var (
 	mapTable = make(map[int]int, 10)
+	//添加一个全局的互斥锁
+	lock sync.Mutex
 )
 
 func test(limit int) {
@@ -19,15 +23,19 @@ func test(limit int) {
 	for i := 1; i <= limit; i++ {
 		res *= i
 	}
+	//加锁
+	lock.Lock()
 	mapTable[limit] = res
+	//解锁
+	lock.Unlock()
 }
 
 func main() {
-	for i := 1; i <= 100; i++ {
+	for i := 1; i <= 20; i++ {
 		go test(i)
 	}
 	//设置来防止协程还未全部开启或完成任务，主线程就执行完成，导致协程也一并销毁
-	time.Sleep(time.Second * 20)
+	time.Sleep(time.Second * 3)
 
 	for k, v := range mapTable {
 		fmt.Printf("map[%d]=%v\n", k, v)
