67687e761dfd0d6145f8508e77f71c66858a2a88
yyn <1601530253@qq.com>
2022-03-20 22:26:52 +0800

    3-20

--- 23-反射/01-反射基本概念/main.go
+++ 23-反射/01-反射基本概念/main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"reflect"
+)
+
 /*
 基本介绍
 	1)反射可以在运行时动态获取变量的各种信息，比如变量的类型(type)，类别(kind)
@@ -7,3 +12,17 @@ package main
 	3)通过反射，可以修改变量的值，可以调用关联的方法。
 	4)使用反射，需要import ("reflect")
 */
+func test(b interface{}) {
+	rType := reflect.TypeOf(b)
+	fmt.Println("rType=", rType)
+
+	rVal := reflect.ValueOf(b)
+	fmt.Println("rType=", rVal)
+	n1 := 10 + rVal.Int()
+	fmt.Println("n1=", n1)
+
+}
+func main() {
+	var num int = 8
+	test(num)
+}
