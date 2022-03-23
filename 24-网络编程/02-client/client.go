e36f0cc4b03a48875b11e0c6bcd7c6f60524d413
yanyinuo <1601530253@qq.com>
2022-03-23 19:19:14 +0800

    3-23

--- 24-网络编程/02-client/client.go
+++ 24-网络编程/02-client/client.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
+	"os"
 )
 
 func main() {
@@ -11,5 +13,18 @@ func main() {
 		fmt.Printf("client Dial err=%v\n", err)
 		return
 	}
-	fmt.Println("Connecting 成功=", conn)
+	//功能一：客户端可以发送单行数据，然后退出
+	reader := bufio.NewReader(os.Stdin)
+
+	//从终端读取一行用户输入，并准备发送给服务器
+	line, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("Error ReadString err=", err)
+	}
+	//再将line发送给 服务器
+	n, err := conn.Write([]byte(line))
+	if err != nil {
+		fmt.Println("Error writing err=", err)
+	}
+	fmt.Printf("客户端发送了%d字节的数据，并退出", n)
 }
