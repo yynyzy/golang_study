af96199f89b58c14723dd25dc7c7464cd9a6158b
yyn <1601530253@qq.com>
2022-03-31 00:09:45 +0800

    3-30

--- 26-海量用户通讯系统/chatroom/common/message/main.go
+++ 26-海量用户通讯系统/chatroom/common/message/main.go
@@ -19,8 +19,8 @@ type Login_Message struct {
 
 //登陆返回的消息
 type Login_Response_Message struct {
-	Code  int   `json:"code"`  //状态码
-	Error error `json:"error"` //错误的类型
+	Code  int    `json:"code"`  //状态码
+	Error string `json:"error"` //错误的类型
 }
 
 //登陆返回的消息
--- 26-海量用户通讯系统/chatroom/server/main.go
+++ 26-海量用户通讯系统/chatroom/server/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/binary"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"golang_study/26-海量用户通讯系统/chatroom/common/message"
 	"io"
@@ -29,8 +30,25 @@ func process(conn net.Conn) {
 	}
 }
 
+//编写一个 ServerProcessMes 函数
+//功能:根据客户端发送消息种类不同，决定调用哪个函数来处理
+func serverProcesMes(conn net.Conn, mes *message.Message) (err error) {
+	switch mes.Type {
+	case message.Login_Mes_Type:
+		//处理登陆
+		err = serverProcessLogin(conn, mes)
+	case message.Register_Mes_Type:
+		//处理注册
+		fmt.Println("")
+
+	default:
+		fmt.Println("消息类型不存在，无法处理")
+	}
+	return
+}
+
 //编写一个函数serverProcessLogin函数， 专门处理登录请求
-func serverprocessLogin(conn net.Conn, mes *message.Message) (err error) {
+func serverProcessLogin(conn net.Conn, mes *message.Message) (err error) {
 	//核心代码...
 	//1. 先从mes中取出mes.Data ，并直接反序列化成LoginMes
 	var loginMes message.Login_Message
@@ -39,27 +57,60 @@ func serverprocessLogin(conn net.Conn, mes *message.Message) (err error) {
 		fmt.Println("json. Unmarshal fail err=", err)
 		return
 	}
+	//先声明一个 resMes
+	var resMes message.Message
+	resMes.Type = message.Login_Res_Mes_Type
+
+	//再声明一个 loginResMes
+	var loginResMes message.Login_Response_Message
+
 	//如果用户id= 100,密码=123,认为合法，否则不合法
 	if loginMes.UserId == 100 && loginMes.UserPwd == "123456" {
 		//合法
+		loginResMes.Code = 200
 	} else {
+		//不合法,返回500
+		loginResMes.Code = 500
+		loginResMes.Error = "该用户不存在，请注册后再登陆..."
 	}
+	//3将loginResMes 序列化
+	data, err := json.Marshal(loginResMes)
+	if err != nil {
+		fmt.Println("json.Marshal fail", err)
+		return
+	}
+	//4.将data赋值给resMes
+	fmt.Println("data", data)
+	fmt.Println("stringdata", string(data))
+	resMes.Data = string(data)
+	//5.对resMes 进行序列化，准备发送
+	data, err = json.Marshal(resMes)
+	if err != nil {
+		fmt.Println("json . Marshal fail", err)
+		return
+	}
+	//6.发送
+	err = writePkg(conn, data)
 	return
 }
 
-//编写一个 ServerProcessMes 函数
-//功能:根据客户端发送消息种类不同，决定调用哪个函数来处理
-func serverProcesMes(conn net.Conn, mes *message.Message) (err error) {
-	switch mes.Type {
-	case message.Login_Mes_Type:
-		//处理登陆
-		fmt.Println("")
-	case message.Register_Mes_Type:
-		//处理注册
-		fmt.Println("")
+func writePkg(conn net.Conn, data []byte) (err error) {
+	var pkglen uint32 = uint32(len(data))
+	var buf [4]byte //4字节的buf用来储存最多32位无符号位的数据
+	binary.BigEndian.PutUint32(buf[:4], pkglen)
+	//发送长度
+	n, err := conn.Write(buf[:4])
+	if n != 4 || err != nil {
+		err = errors.New("conn.Write(bytes) fail err")
+		return
+	}
+	// fmt.Printf("客户端发送信息的长度=%d,内容是=%s\n", pkglen, string(data))
 
-	default:
-		fmt.Println("消息类型不存在，无法处理")
+	//发送消息本身
+	n, err = conn.Write(data)
+	if n != int(pkglen) || err != nil {
+		err = errors.New("conn.Write(data) fail err")
+		return
 	}
 	return
 }
