50104798f0e842988ed320f3d2c5d60115f99b03
yyn <1601530253@qq.com>
2022-04-03 15:30:57 +0800

    4-3

--- 26-海量用户通讯系统/chatroom/common/message/message.go
+++ 26-海量用户通讯系统/chatroom/common/message/message.go
@@ -1,9 +1,10 @@
 package message
 
 const (
-	Login_Mes_Type     = "Login_Message"
-	Login_Res_Mes_Type = "Login_Response_Message"
-	Register_Mes_Type  = "Register_Message"
+	Login_Mes_Type        = "Login_Message"
+	Login_Res_Mes_Type    = "Login_Response_Message"
+	Register_Mes_Type     = "Register_Message"
+	Register_Res_Mes_Type = "Register_Response_Message"
 )
 
 type Message struct {
@@ -25,4 +26,10 @@ type Login_Response_Message struct {
 
 //登陆返回的消息
 type Register_Message struct {
+	User User `json:"user"` //类型就是User结构体
+}
+
+type Register_Response_Message struct {
+	Code  int    `json:"code"`  //状态码400表示用户已占有，200表示注册成功
+	Error string `json:"error"` //错误的类型
 }
