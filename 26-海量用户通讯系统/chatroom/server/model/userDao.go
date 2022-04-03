55ed6826fd9bb65764308996ec886e6e3400a7ad
yyn <1601530253@qq.com>
2022-04-03 17:17:53 +0800

    4-3

--- 26-海量用户通讯系统/chatroom/server/model/userDao.go
+++ 26-海量用户通讯系统/chatroom/server/model/userDao.go
@@ -61,3 +61,26 @@ func (this *UserDao) Login(userId int, userPwd string) (user *User, err error) {
 	}
 	return
 }
+func (this *UserDao) Register(user *User) (err error) {
+	//从链接池中取一个链接
+	conn := this.pool.Get()
+
+	defer conn.Close() //关闭这个连接
+	//判断 redis 中 id 是否存在
+	_, err = this.getUserById(conn, user.UserId)
+	//如果没有错误，说明redis中存在id，说明用户id已经被注册过
+	if err == nil {
+		err = ERROR_USER_EXISTS
+		return
+	}
+	data, err := json.Marshal(user) //将传入的注册信息序列化
+	if err != nil {
+		return
+	}
+	_, err = conn.Do("HSet", "users", user.UserId, string(data))
+	if err != nil {
+		fmt.Println("用户注册保存发生错误 err=", err)
+		return
+	}
+	return
+}
