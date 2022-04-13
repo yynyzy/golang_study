76e261fa34d406b8915e7d1ea9af863e055e4a95
yyn <1601530253@qq.com>
2022-04-13 16:51:32 +0800

    4-13

--- 26-海量用户通讯系统/chatroom/client/process/smsProcess.go
+++ 26-海量用户通讯系统/chatroom/client/process/smsProcess.go
@@ -1,1 +1,45 @@
 package process
+
+import (
+	"encoding/json"
+	"fmt"
+	"golang_study/26-海量用户通讯系统/chatroom/client/utils"
+	"golang_study/26-海量用户通讯系统/chatroom/common/message"
+)
+
+type SmsProcess struct {
+}
+
+func (this *SmsProcess) SendGroupMessage(Content string) (err error) {
+	//创建一个mes
+	var mes message.Message
+	mes.Type = message.SmsMes_Type
+
+	//创建一个 smsMes实例
+	var smsMes message.SmsMes
+	smsMes.Content = Content
+	smsMes.UserId = CurUser.UserId
+	smsMes.UserStatus = CurUser.UserStatus
+
+	data, err := json.Marshal(smsMes)
+	if err != nil {
+		fmt.Println("SendGroupMessage json.Marshal(smsMes) err=", err.Error())
+		return
+	}
+	mes.Data = string(data)
+
+	data, err = json.Marshal(mes)
+	if err != nil {
+		fmt.Println("SendGroupMessage json.Marshal(mes) err=", err.Error())
+		return
+	}
+	tf := &utils.Transfer{
+		Conn: CurUser.Conn,
+	}
+	err = tf.WritePkg(data)
+	if err != nil {
+		fmt.Println("SendGroupMessage tf.WritePkg(data) err=", err.Error())
+		return
+	}
+	return
+}
