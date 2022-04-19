12dee9c5001a80a3d278d869fe760ea7cd0230b4
yanyinuo <1601530253@qq.com>
2022-04-19 16:27:29 +0800

    4-19

--- 27-数据结构与算法/02-队列/02-队列.go
+++ 27-数据结构与算法/02-队列/02-队列.go
@@ -1,11 +1,15 @@
 package main
+
 import (
-	fmt
+	"errors"
+	"fmt"
+	"os"
 )
+
 //使用一个结构体管理队列
 type Queue struct {
 	maxSize int
-	array   [4]int //数组=>模拟队列
+	array   [5]int //数组=>模拟队列
 	front   int    //表示指向队列首
 	rear    int    //表示指向队列的尾部
 }
@@ -26,6 +30,19 @@ func (this *Queue) showQueue() {
 	for i := this.front + 1; i <= this.rear; i++ {
 		fmt.Printf("array[%d]=%d\t", i, this.array[i])
 	}
+	fmt.Println("")
+}
+
+//从队列中取出数据
+func (this *Queue) GetQueue() (val int, err error) {
+	//先判断队列是否为空
+	if this.rear == this.front {
+		//队空
+		return -1, errors.New("queue empty")
+	}
+	this.front++
+	val = this.array[this.front]
+	return val, err
 }
 
 //编写一个主函数测试,测试
@@ -37,15 +54,37 @@ func main() {
 		rear:    -1,
 	}
 	var key string
+	var val int
 	for {
 		fmt.Println("1.输入add表示添加数据到队列")
 		fmt.Println("2.输入get表示从队列获取数据")
 		fmt.Println("3.输入show表示显示队列")
 		fmt.Println("4.输入exit表示显示队列")
-		fmt.scanln(&key)
+		fmt.Scanln(&key)
 		switch key {
 		case "add":
 			fmt.Println("输入你要入队列数")
+			fmt.Scanln(&val)
+			err := queue.AddQueue(val)
+			if err != nil {
+				fmt.Println(err.Error())
+			} else {
+				fmt.Println("加入队列ok")
+			}
+		case "get":
+			val, err := queue.GetQueue()
+			if err != nil {
+				fmt.Println(err.Error())
+			} else {
+				fmt.Println("从队列中取出了一个数=", val)
+			}
+
+		case "show":
+			queue.showQueue()
+
+		case "exit":
+			os.Exit(0)
 		}
+
 	}
 }
