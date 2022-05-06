d71938d91a1e6b68754443ec1d588bf08d0160ae
yanyinuo <1601530253@qq.com>
2022-05-06 18:35:19 +0800

    5-6

--- 27-数据结构与算法/08-递归/main.go
+++ 27-数据结构与算法/08-递归/main.go
@@ -3,11 +3,30 @@ package main
 import "fmt"
 
 func main() {
-	exp := "ch+"
-	exp2 := "c"
-	exp1 := exp[0:1]
-	fmt.Printf("%T,%s\n", exp, exp)
-	fmt.Printf("%T,%s\n", exp2, exp2)
-	fmt.Printf("%T,%s\n", exp1, exp1)
-	fmt.Println([]byte(exp1))
+
+	//先创建一个二维数组，模拟迷宫
+	//规则
+	//1.如果元素的值为缸，就是墙
+	//2．如果元素的值为e，是没有走过的点
+	//3．如果元素的值为2，是一个通路
+	//4．如果元素的值为3,是走过的点,但是走不通
+	var myMap [8][7]int
+
+	//先把地图的最上和最下设置为1
+	for i := 0; i < 7; i++ {
+		myMap[0][i] = 1
+		myMap[7][i] = 1
+	}
+	//先把地图的最左和最右设置为1
+	for i := 0; i < 8; i++ {
+		myMap[i][0] = 1
+		myMap[i][6] = 1
+	}
+	//输出地图
+	for i := 0; i < 8; i++ {
+		for j := 0; j < 7; j++ {
+			fmt.Print(myMap[i][j], " ")
+		}
+		fmt.Println()
+	}
 }
