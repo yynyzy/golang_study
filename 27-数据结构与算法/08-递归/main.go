a1d3a30c41e1f2cb59e87d774c428653cce66cf3
yanyinuo <1601530253@qq.com>
2022-05-06 20:16:39 +0800

    5-6

--- 27-数据结构与算法/08-递归/main.go
+++ 27-数据结构与算法/08-递归/main.go
@@ -2,14 +2,44 @@ package main
 
 import "fmt"
 
+//编写一个函数，完成老鼠找路
+//myMap *[8][7]int:地图， 保证是同一个地图，使用引用
+//i,j表示对地图的哪个点进行测试
+func SetWay(myMap *[8][7]int, i int, j int) bool {
+	//分析出什么情况下，就找到出路
+	//myMap[6][5] == 2
+	if myMap[6][5] == 2 {
+		return true
+	} else {
+		//说明要继续找
+		if myMap[i][j] == 0 { //如果这个点是可以探测
+			//假设这个点是可以通，但是需要探测上下左右
+			myMap[i][j] = 2
+			if SetWay(myMap, i-1, j) { // 上
+				return true
+			} else if SetWay(myMap, i+1, j) { //下
+				return true
+			} else if SetWay(myMap, i, j-1) { //左
+				return true
+			} else if SetWay(myMap, i, j+1) { //右
+				return true
+			} else { //死路
+				myMap[i][j] = 3
+				return false
+			}
+		} else { //是1,为墙
+			return false
+		}
+	}
+}
 func main() {
 
 	//先创建一个二维数组，模拟迷宫
 	//规则
-	//1.如果元素的值为缸，就是墙
-	//2．如果元素的值为e，是没有走过的点
-	//3．如果元素的值为2，是一个通路
-	//4．如果元素的值为3,是走过的点,但是走不通
+	//1. 如果元素的值为 1，就是墙
+	//2．如果元素的值为 0，是没有走过的点
+	//3．如果元素的值为 2，是一个通路
+	//4．如果元素的值为 3,是走过的点,但是走不通
 	var myMap [8][7]int
 
 	//先把地图的最上和最下设置为1
