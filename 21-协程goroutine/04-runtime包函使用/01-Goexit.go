c7092d7f693f9ebeba52ab3bc080fd03fcdb492e
yanyinuo <1601530253@qq.com>
2022-05-29 23:18:20 +0800

    5-29

---
+++ 21-协程goroutine/04-runtime包函使用/01-Goexit.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+)
+
+func say(s string) {
+
+	for i := 0; i < 2; i++ {
+
+		runtime.Gosched()
+
+		fmt.Println(s)
+
+	}
+
+}
+
+func main() {
+
+	go say("world")
+
+	say("hello")
+	/*
+		hello
+		  world
+		  hello
+	*/
+}
