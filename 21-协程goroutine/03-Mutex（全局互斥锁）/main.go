aecc9d5cce379eff0bc979ec3552e2e9358464c6
yanyinuo <1601530253@qq.com>
2022-06-03 22:42:55 +0800

    6-3

--- 21-协程goroutine/03-Mutex（全局互斥锁）/main.go
+++ 21-协程goroutine/03-Mutex（全局互斥锁）/main.go
@@ -23,7 +23,7 @@ func test(limit int) {
 	for i := 1; i <= limit; i++ {
 		res *= i
 	}
-	//加锁
+	//加锁,防止同时对mapTable操作
 	lock.Lock()
 	mapTable[limit] = res
 	//解锁
@@ -34,8 +34,9 @@ func main() {
 	for i := 1; i <= 20; i++ {
 		go test(i)
 	}
+
 	//设置来防止协程还未全部开启或完成任务，主线程就执行完成，导致协程也一并销毁
-	time.Sleep(time.Second * 3)
+	time.Sleep(time.Second)
 
 	for k, v := range mapTable {
 		fmt.Printf("map[%d]=%v\n", k, v)
