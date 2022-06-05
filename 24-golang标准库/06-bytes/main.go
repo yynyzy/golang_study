a7a61d98d755516c32bf64226d16f9e015f22aae
yanyinuo <1601530253@qq.com>
2022-06-06 00:03:01 +0800

    6-5

--- 24-golang标准库/06-bytes/main.go
+++ 24-golang标准库/06-bytes/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"strings"
+)
 
 func test() {
 	var i int = 100
@@ -17,8 +21,21 @@ func test() {
 	b1 = []byte(s)
 	fmt.Println(s)
 	fmt.Println(b1)
+}
+
+func test2() {
+	s := "duoke360. com"
+	b := []byte(s)
+	b1 := []byte("duoke360")
+	b2 := []byte("DuoKe360")
+	strings.Contains("hello world", "hello")
+	b3 := bytes.Contains(b, b1)
+	fmt.Printf("b3: %v\n", b3)
+	b3 = bytes.Contains(b, b2)
+	fmt.Printf("b3: %v\n", b3)
 
 }
 func main() {
-	test()
+	// test()
+	test2()
 }
