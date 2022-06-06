8aba9315d53f2f261a6ebb8c14c853a081fe2c33
yanyinuo <1601530253@qq.com>
2022-06-06 22:16:36 +0800

    6-6

--- 24-golang标准库/09-time包/main.go
+++ 24-golang标准库/09-time包/main.go
@@ -19,6 +19,31 @@ func test1() {
 	fmt.Printf("%T,%T,%T,%T,%T,%T,%T\n", now, year, month, day, hour, minute, second)
 	// time.Time, int, time.Month, int, int, int, int
 }
+
+//获取时间戳
+func test2() {
+	now := time.Now()
+	//当前时间戳 TimeStamp type :int64，TimeStamp : 1654507225
+	fmt.Printf("TimeStamp type:%T,TimeStamp: %v", now.Unix(), now.Unix())
+}
+
+func test3() {
+	//使用time.Tick(时间间隔)来设置定时器，定时器的本质上是一个通道(channel)。
+	ticker := time.Tick(time.Second)
+	// 等价于 ticker2 := time.NewTicker(time.Second)
+	for i := range ticker {
+		fmt.Println(i)
+	}
+}
+
+func test4() {
+	t := time.Now()
+	fmt.Println(t.Format("2006/01/0215:04"))
+
+}
 func main() {
-	test1()
+	// test1()
+	// test2()
+	// test3()
+	test4()
 }
