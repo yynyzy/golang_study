25f1726d133929e7fbad8fb6766aa9c5934bd83c
yanyinuo <1601530253@qq.com>
2022-06-06 22:37:51 +0800

    6-6

---
+++ 19-JSON序列化/03-Decode/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+func test6() {
+	f, _ := os.Open("go_study\\19-JSON序列化\\03-Decode\\a.json")
+	defer f.Close()
+	d := json.NewDecoder(f)
+	var v map[string]interface{}
+	d.Decode(&v)
+	fmt.Printf("v : %v\n", v)
+}
+func main() {
+	test6()
+}
