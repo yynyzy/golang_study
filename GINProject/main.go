fa68b1e0371c3d7cc8fec1f5e77fea416923532b
yyn <1601530253@qq.com>
2022-06-28 15:23:32 +0800

    6-27changes

--- GINProject/main.go
+++ GINProject/main.go
@@ -3,11 +3,10 @@ package main
 import "github.com/gin-gonic/gin"
 
 func main() {
+	//创建一个默认的路由引擎
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
+	r.GET("/", func(c *gin.Context) {
+		c.String(200, "值:%v", "你好GIN")
 	})
 	r.Run() // 监听并在 0.0.0.0:8080 上启动服务
 }
